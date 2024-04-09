56cb23c10cd28bfe428e111dacd81ae429c14e61
ruairi keane <ruairi.rk@icloud.com>
2024-04-09 15:46:00 +0000

    Change secret key

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fl8nw0hnkmv(y=_$h7j6+6(a15i66!(1dy!-h*zz0jm@*_3rm+'
+SECRET_KEY = os.environ.get("a1b2c3")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-ruairikeane1-djangoblog-dlgic2xtbo6.ws-eu110.gitpod.io'
 ,'.herokuapp.com']

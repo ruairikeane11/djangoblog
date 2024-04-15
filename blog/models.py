d36c53dc571dbf914d5ebf1d263d8798cb9bef95
ruairi keane <ruairi.rk@icloud.com>
2024-04-15 11:06:41 +0000

    Debug comment model

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,9 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
-
+    
+    
+    
     class Meta:
         ordering = ["-created_on"]
 
@@ -33,6 +35,7 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    challenge = models.FloatField(default=3.0)
 
     class Meta:
         ordering = [ 'created_on' ]

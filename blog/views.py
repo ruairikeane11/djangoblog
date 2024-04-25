0970c70f5f062802c8a337342e7e1ffc410b727d
ruairi keane <ruairi.rk@icloud.com>
2024-04-25 09:35:33 +0000

    enable serving of image files

--- blog/views.py
+++ blog/views.py
@@ -2,23 +2,21 @@ from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
 from django.http import HttpResponseRedirect
-from django.contrib.auth.models import User
 from .models import Post, Comment
 from .forms import CommentForm
 
-
-
 # Create your views here.
 
+
 class PostList(generic.ListView):
+  
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
 
 
-
 def post_detail(request, slug):
-
+    
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
@@ -33,31 +31,24 @@ def post_detail(request, slug):
             messages.add_message(
                 request, messages.SUCCESS,
                 'Comment submitted and awaiting approval'
-                )
+            )
 
     comment_form = CommentForm()
-    
+
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post,
-         "comments": comments,
-         "comment_count": comment_count,
-         "comment_form": comment_form,
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form
         },
     )
 
 
-def profile_page(request):
-    user = get_object_or_404(User, user=request.user)
-    # Retrieve all comments for the user object
-    comments = user.commenter.all()
-
-
 def comment_edit(request, slug, comment_id):
-    """
-    view to edit comments
-    """
+    
     if request.method == "POST":
 
         queryset = Post.objects.filter(status=1)
@@ -72,15 +63,14 @@ def comment_edit(request, slug, comment_id):
             comment.save()
             messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
         else:
-            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+            messages.add_message(request, messages.ERROR,
+                                 'Error updating comment!')
 
     return HttpResponseRedirect(reverse('post_detail', args=[slug]))
 
 
 def comment_delete(request, slug, comment_id):
-    """
-    view to delete comment
-    """
+  
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
     comment = get_object_or_404(Comment, pk=comment_id)
@@ -89,7 +79,7 @@ def comment_delete(request, slug, comment_id):
         comment.delete()
         messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
     else:
-        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+        messages.add_message(request, messages.ERROR,
+                             'You can only delete your own comments!')
 
     return HttpResponseRedirect(reverse('post_detail', args=[slug]))
-    
--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-ruairikeane1-djangoblog-dlgic2xtbo6.ws-eu110.gitpod.io'
 ,'.herokuapp.com']
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
+    'cloudinary',
     'blog',
     'about',
 ]

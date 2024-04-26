f8478de0379407e1aaf3b16e15ed1e27251d7077
ruairi keane <ruairi.rk@icloud.com>
2024-04-26 12:54:57 +0000

    Add final tests

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-ruairikeane1-djangoblog-dlgic2xtbo6.ws-eu110.gitpod.io'
 ,'.herokuapp.com']
